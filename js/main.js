c9c3bc631900bd5a676bcdb5138006c51dc15f22
feafania <173483105+feafania@users.noreply.github.com>
2026-08-24 22:39:32 +0100

    feat: add init sidebar functionality

--- js/main.js
+++ js/main.js
@@ -17,7 +17,54 @@ $(document).ready(function(){
       pauseOnHover: true,
       pauseOnFocus: true,
 
-      speed: 500
+      adaptiveHeight: true,
+
+      speed: 300
     }
   );
+
+  $('#side-menu-placeholder').load( "./side-menu.html" ,
+    function( response, status, xhr ) {
+        if ( status === "error" ) {
+            console.error("Unable to load side-menu.html: " + xhr.status + " " + xhr.statusText)
+        } else if ( status === "success" || status === "notmodified" ) {
+          initSidebar();
+        }
+    })
 });
+
+function initSidebar () {
+  $(document).on("click", '[data-toggle="sidebar"]', function (e) {
+    e.stopPropagation();
+    $("body").toggleClass("sidebar-open");
+    $(this).toggleClass("is-active");
+  });
+
+  $(document).on("click", '#container', function () {
+    if ($("body").hasClass("sidebar-open")) {
+      $("body").removeClass("sidebar-open");
+      $('[data-toggle="sidebar"]').removeClass("is-active");
+    }
+  });
+
+  $(document).on("click", '.sidebar-overlay', function () {
+    if ($("body").hasClass("sidebar-open")) {
+      $("body").removeClass("sidebar-open");
+      $('[data-toggle="sidebar"]').removeClass("is-active");
+    }
+  });
+}
+
+func
+
+// document.addEventListener('DOMContentLoaded', function(){
+//     fetch("./side-menu.html")
+//       .then(response => response.text())
+//       .then((data) => {
+//           const sideMenu = document.getElementById('side-menu-placeholder');
+//           if (sideMenu) {
+//               sideMenu.innerHTML = data;
+//           }
+//       })
+//       .catch((error) => console.error("Unable to load side-menu.html: " + error));
+// })
