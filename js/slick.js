93857b97dac0138dcaa1ca2a6e855a034c13f72b
feafania <173483105+feafania@users.noreply.github.com>
2026-08-26 09:30:20 +0100

    feat: add carousel to logo strip

--- js/slick.js
+++ js/slick.js
@@ -22,4 +22,28 @@ export default function initSlick() {
       speed: 300
     }
   );
+
+    $('.logo-strip__list').slick(
+      {
+          variableWidth: true,
+          slidesToScroll: 1,
+          slidesToShow: 10,
+
+          infinite: true,
+
+          autoplay: true,
+          autoplaySpeed: 3000,
+
+          dots: false,
+          draggable: false,
+          arrows: false,
+
+          pauseOnHover: true,
+          pauseOnFocus: true,
+
+          adaptiveHeight: true,
+
+          speed: 250
+      }
+    );
 }
